4ba4e9625c84c91e50989b608dc2ba83adc44d24
Soliheen Farooq Khan <soliheen.farooqkhan@u-next.com>
2025-04-06 22:36:52 +0530

    addedLimitOffsetForUsersApi

--- lib/api/src/models/common.rs
+++ lib/api/src/models/common.rs
@@ -1,10 +1,14 @@
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct FieldSelection {
     pub fields: Option<String>, // e.g. "id,name,email"
     pub email: Option<String>,
     pub name: Option<String>,
     pub role: Option<String>,
     pub phone_number: Option<String>,
+    pub limit: Option<i32>,
+    #[serde(rename = "skip")]
+    pub off_set: Option<i32>,
 }
